306a8b7d73cce34e6a42372be62cd73d8c3e20b3
alexzaignacio <alexzagayleadaoignacio@gmail.com>
2025-12-27 09:00:09 +0800

    Implement OnlineMonitor class with display update methods

--- src/OnlineMonitor.java
+++ src/OnlineMonitor.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class OnlineMonitor {
+    private int currentDisplayed;
+
+    public OnlineMonitor() {
+        this.currentDisplayed = 0;
+    }
+
+    public void updateDisplay(int number) {
+        this.currentDisplayed = number;
+        System.out.println("Online Monitor updated: Now displaying #" + currentDisplayed);
+    }
+
+    public int getDisplay() {
+        return currentDisplayed;
+    }
 }
