ee7e003b494d08353001788fe9da30ae2af19dbc
alexzaignacio <alexzagayleadaoignacio@gmail.com>
2025-12-27 09:00:32 +0800

    Implement HelpDeskStation class with customer processing

--- src/HelpDeskStation.java
+++ src/HelpDeskStation.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class HelpDeskStation {
+    private String stationId;
+    private QueueSystem queueSystem;
+
+    public HelpDeskStation(String stationId, QueueSystem queueSystem) {
+        this.stationId = stationId;
+        this.queueSystem = queueSystem;
+    }
+
+    public void processNextCustomer() {
+        if (queueSystem.getCurrentNumber() > 0) {
+            System.out.println("Station " + stationId + " processing customer #" + queueSystem.getCurrentNumber());
+            queueSystem.advanceQueue();
+        } else {
+            System.out.println("Station " + stationId + ": No customer in queue yet.");
+        }
+    }
+
+    public void resetQueue(int newNumber) {
+        System.out.println("Station " + stationId + " requesting queue reset.");
+        queueSystem.resetQueueNumber(newNumber);
+    }
+
+    public String getStationId() {
+        return stationId;
+    }
 }
